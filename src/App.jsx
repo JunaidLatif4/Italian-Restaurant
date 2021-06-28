ec90c3967ee4e6157e46f8b89088fb7d8e5ebd22
Junaid Latif <junaidlatif004@gmail.com>
2021-06-28 19:22:46 +0500

    Creating Home Page

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Route , Switch , Redirect} from 'react-router-dom'
+
+import Header from './Components/Header'
+
+import './App.scss'
+
+const App = ()=>{
+  return(
+    <>
+      <Header/>
+      <Switch>
+
+      </Switch>
+    </>
+  )
+}
+
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="header_container">
+            <div className="header_box">
+                
+            </div>
+            
+        </div>
+    )
+}
+
+export default Header
